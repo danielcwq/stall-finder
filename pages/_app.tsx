f0288d1b53e0592b37046f829c194934cadbd8f5
Daniel Ching <dancwq2@gmail.com>
2025-03-07 08:52:43 +0800

    header changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import GoogleAnalytics from '../components/GoogleAnalytics'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <title>Ho Jiak Bo</title>
+                <link rel="icon" href="/output-onlinegiftools.gif" type="image/gif" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Find delicious food stalls from Singapore food blogs near you" />
+            </Head>
             <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
             <Component {...pageProps} />
         </>
